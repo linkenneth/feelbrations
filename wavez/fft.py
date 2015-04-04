ff2ccc1fdc110be0f3579ad8683c2fe683407cdc
Kevin Chen <calmthatwombat>
2015-04-04 13:19:12 -0700

    up to the point that fft algorithm used

--- wavez/fft.py
+++ wavez/fft.py
@@ -1,4 +1,6 @@
 import argparse
+from pydub import AudioSegment
+
 from pylab import *
 from scipy.io import wavfile
 
@@ -8,10 +10,23 @@ def main():
                         help='file to play')
     args = parser.parse_args()
 
-    sampFreq, snd = wavfile.read(args.filename)
-    snd = snd / (2.0**15)
-    s1 = snd[:,0]               # pick first channel out of the two
-    
+    # if .mp3, convert to .wav
+    s = args.filename.split('.')
+    prefix, ext = '.'.join(s[:-1]), s[-1]
+    if ext == 'wav':
+        song = AudioSegment.from_wav(args.filename)
+    elif ext == 'mp3':
+        song = AudioSegment.from_mp3(args.filename)
+    else:
+        raise SyntaxError('SHIeeeT wrong file type')
+    song_name = '{}_converted.wav'.format(prefix)
+    song.export(song_name, format='wav')
+
+    # administer fft
+    sampFreq, snd = wavfile.read(song_name) # default int16 values
+    snd = snd / (2.0**15)     # normalize into -1.0 to 1.0 value range
+    s1 = snd[:, 0]            # only use first channel out of the two
+
     n = len(s1)
     p = fft(s1)
     
