6afaf2340eed2f9594f04db2f54b46f68c66f97a
Kenneth Lin <kenlin@berkeley.edu>
2015-04-05 11:37:17 -0700

    fixed some out of sync errors

--- motor-v2/motor_v2/motor_v2.ino
+++ motor-v2/motor_v2/motor_v2.ino
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 
-#define MOTORS_COUNT 9
+#define MOTORS_COUNT 8
 #define CLOCK_RATE pow(2, 24)
+#define MOTOR_FREQ 50
 
-int motors[MOTORS_COUNT] = { 2, 3, 4, 5, 6, 7, 24, 28, 29 };
+int motors[MOTORS_COUNT] = { 5, 4, 3, 2, 25, 46, 48, 44 };
+// 44 red
 
 typedef struct event {
   unsigned long time;  // microseconds
@@ -28,18 +30,28 @@ int event_compare(const void *a, const void *b) {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  for (int i = 0; i < MOTORS_COUNT; i++) {
+    pinMode(motors[i], OUTPUT);
+  }
+  // OUTPUT THEN OFF TO BE SAFE
+  pinMode(6, OUTPUT);
+  digitalWrite(6, LOW);
+  pinMode(24, OUTPUT);
+  digitalWrite(24, LOW);
+  pinMode(29, OUTPUT);
+  digitalWrite(29, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long start = micros();
+  unsigned long start = 0;
   char buffer[2];
   int i = 0;
 
-  // turn all the motors on
-  for (i = 0; i < MOTORS_COUNT; i++) {
-    digitalWrite(motors[i], HIGH);
-  }
+//  // turn all the motors on
+//  for (i = 0; i < MOTORS_COUNT; i++) {
+//    digitalWrite(motors[i], HIGH);
+//  }
 
   // read power levels, determine when to turn motors off
   i = 0;
@@ -48,16 +60,20 @@ void loop() {
       // Computer will send 9 power levels (0-100), one for each motor.
       // Here, calculate the duty for each motor's duty cycle based on
       // this value.
-      Serial.println("Reading data from serial...");
+//      Serial.println("Reading data from serial...");
       Serial.readBytes(buffer, 2);
       unsigned int power = atoi(buffer);
-      Serial.print("Power read requested: ");
-      Serial.println(power);
-      unsigned long duty = (unsigned long) CLOCK_RATE * power / 100.0;
+//      Serial.print("Power read requested: ");
+//      Serial.println(power);
+      unsigned long duty = (unsigned long) CLOCK_RATE * power / 100.0 / MOTOR_FREQ;
       Event event = { duty, i, LOW };  // only need LOW events
-      Serial.print("Calculated duty is ");
-      Serial.println(duty);
+//      Serial.print("Calculated duty is ");
+//      Serial.println(duty);
       schedule[i++] = event;
+      
+      if (start == 0) {
+        start = micros();
+      }
     }
   }
 
@@ -67,13 +83,18 @@ void loop() {
   for (i = 0; i < MOTORS_COUNT; i++) {
     Event event = schedule[i];
     long sleep_time = event.time - (micros() - start);
-    Serial.print(event.motor_id);
-    Serial.print(" stopping after ");
-    Serial.println(event.time);
+//    Serial.print(motors[event.motor_id]);
+//    Serial.print(" stopping after ");
+//    Serial.println(event.time);
     if (sleep_time > 0) {
+      digitalWrite(motors[event.motor_id], HIGH);
       delayMicroseconds(sleep_time);
+      digitalWrite(motors[event.motor_id], event.new_state);
     }
-
-    digitalWrite(motors[event.motor_id], event.new_state);
+  }
+  
+  long sleep_time = CLOCK_RATE / MOTOR_FREQ - (micros() - start);
+  if (sleep_time > 0) {
+    delayMicroseconds(sleep_time);
   }
 }
